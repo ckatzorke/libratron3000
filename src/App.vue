6ba0bac9726e024c348c0ec567baccf1df33a699
Christian Katzorke <ckatzorke@gmail.com>
2019-03-25 22:31:47 +0100

    moves to vue cli 3

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,122 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+  <div id="app">
+     <v-app id="inspire" light>
+      <v-navigation-drawer
+        clipped
+        fixed
+        v-model="drawer"
+        :mini-variant="mini"
+        hide-overlay
+        stateless
+        app
       >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+        <v-toolbar flat class="transparent">
+          <v-list class="pa-0">
+            <v-list-tile avatar>
+              <v-btn
+                icon
+                @click.stop="mini = !mini"
+              >
+                <v-icon>{{ menuFlyoutDirection }}</v-icon>
+              </v-btn>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+              <v-list-tile-content>
+                <v-list-tile-title>Choose your fate</v-list-tile-title>
+              </v-list-tile-content>
+
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+
+        <v-list class="pt-0">
+          <v-divider></v-divider>
+          <v-list-tile @click.stop="go('/')">
+            <v-list-tile-action>
+              <v-icon>dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                Home
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile  @click.stop="go('/collection')">
+            <v-list-tile-action>
+              <v-icon>list</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              View collection
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile  @click.stop="go('/import')">
+            <v-list-tile-action>
+              <v-icon>input</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              Import Collection
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app fixed clipped-left>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Libratron3000</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat @click.stop="go('/profile')"><v-icon>account_circle</v-icon></v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid fill-height>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+      <v-footer app fixed>
+        <span>&copy; 2019</span>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Home from './components/Home.vue'
+import Collection from './components/Collection.vue'
+import Import from './components/Import.vue'
+
+import firebase from 'firebase'
 
 export default {
-  name: 'App',
+  name: 'libratron3000',
   components: {
-    HelloWorld
+    'lib-home': Home,
+    'lib-collection': Collection,
+    'lib-import': Import
   },
   data () {
     return {
-      //
+      drawer: true,
+      mini: true
     }
+  },
+  props: {
+    source: String
+  },
+  methods: {
+    go(path) {
+      this.$router.push(path)
+    }
+  },
+  computed: {
+    menuFlyoutDirection() {
+      return this.mini ? 'chevron_right' : 'chevron_left'
+    }
+  },
+  created() {
+    console.log('Created... Logging in.')
+    this.$store.dispatch('bootstrapLogin')
   }
 }
 </script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import Vuetify from 'vuetify'
-import { router } from './routes'
+import { router } from './router'
 import { store } from './store/store'
 import firebase from 'firebase'
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBUVKVrviQcGO7Po1cb16X538jufZ-o9dA",
-  authDomain: "piletron3000.firebaseapp.com",
-  databaseURL: "https://piletron3000.firebaseio.com",
-  projectId: "piletron3000",
-  storageBucket: "piletron3000.appspot.com",
-  messagingSenderId: "302651587220"
-};
-firebase.initializeApp(config);
+  apiKey: 'AIzaSyBUVKVrviQcGO7Po1cb16X538jufZ-o9dA',
+  authDomain: 'piletron3000.firebaseapp.com',
+  databaseURL: 'https://piletron3000.firebaseio.com',
+  projectId: 'piletron3000',
+  storageBucket: 'piletron3000.appspot.com',
+  messagingSenderId: '302651587220'
+}
+firebase.initializeApp(config)
 
 // initialize Vue
 Vue.use(Vuetify)
