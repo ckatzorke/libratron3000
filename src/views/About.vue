f515a7f104e8063fd9c95c480a84f1fd8f5f9cef
Christian Katzorke <ckatzorke@gmail.com>
2019-05-26 11:39:54 +0200

    more about info

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,7 @@
         <div class="pa-1 ma-2">
           <p class="display-1 font-weight-medium">About libratron3000 &trade;</p>
           <p>Libratron is a private project by Christian Katzorke.</p>
+          <p>Built with the power of</p>
           <ul>
             <li>
               <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue</a>
@@ -15,7 +16,15 @@
             <li>
               <a href="https://github.com/craigh411/vue-star-rating" target="_blank" rel="noopener">Vue Star Rating</a>
             </li>
-
+          </ul>
+          <p class="my-2">Game information powered by</p>
+          <ul>
+            <li>
+              <a href="https://www.igdb.com/" target="_blank" rel="noopener">IGDB</a>
+            </li>
+            <li>
+              <a href="https://www.collectorz.com/game" target="_blank" rel="noopener">CLZ Game Collector</a>
+            </li>
           </ul>
         </div>
       </v-card>
