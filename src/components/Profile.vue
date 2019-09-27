746063738de0360e358c68a314e96cd8f6905730
Christian Katzorke <ckatzorke@gmail.com>
2019-09-27 09:08:20 +0200

    more dashboard widgets

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -12,6 +12,18 @@
           </v-card-text>
 
           <v-card-actions>
+            <v-btn
+              to="/collection"
+              class="my-2"
+            >
+              <v-icon>list</v-icon> collection
+            </v-btn>
+            <v-btn
+              to="/add"
+              class="my-2"
+            >
+              <v-icon>add</v-icon> Add Games
+            </v-btn>
             <v-spacer></v-spacer>
             <v-btn @click="logout">
               Logout<v-icon right>exit_to_app</v-icon>
@@ -19,8 +31,8 @@
           </v-card-actions>
         </v-card>
       </v-flex>
-      <v-flex xs12 sm6>
-        <v-card class="text-center pa-1 ma-3">
+      <v-flex xs12 sm2>
+        <v-card class="text-center pa-1 ma-3 fill-height">
           <v-progress-circular
             v-if="loading"
             :size="70"
@@ -28,25 +40,18 @@
             indeterminate
             class="my-2"
           ></v-progress-circular>
-          <div v-else  class="text-center">
-            <div>
-              <span class="display-4">
-                {{ collectionCount }}
-              </span>
-              <span>Games</span>
+          <div v-else>
+            <div class="body-1 text-center">
+              # of Games
             </div>
-            <div>
-              <v-btn
-                rounded
-                to="/collection"
-                class="my-2"
-              ><v-icon>list</v-icon> Collection</v-btn>
+            <div class="py-2 text-right display-2 orange--text ">
+              {{ collectionCount }}
             </div>
           </div>
         </v-card>
       </v-flex>
-      <v-flex xs12 sm6>
-        <v-card class="text-center pa-1 ma-3">
+      <v-flex xs6 sm2>
+        <v-card class="text-center pa-1 ma-3 fill-height">
           <v-progress-circular
             v-if="loading"
             :size="70"
@@ -54,13 +59,97 @@
             indeterminate
             class="my-2"
           ></v-progress-circular>
-          <v-btn
-            v-else
-            rounded
-            to="/add"
+          <div v-else>
+            <div class="body-1 text-center">
+              Added {{ thisYear }}
+            </div>
+            <div class="py-2 text-right display-2 orange--text ">
+              {{ addedThisYear }}
+            </div>
+          </div>
+        </v-card>
+      </v-flex>
+      <v-flex xs6 sm2>
+        <v-card class="text-center pa-1 ma-3 fill-height">
+          <v-progress-circular
+            v-if="loading"
+            :size="70"
+            :width="7"
+            indeterminate
             class="my-2"
-            >
-            <v-icon>add</v-icon> Add Game</v-btn>
+          ></v-progress-circular>
+          <div v-else>
+            <div class="body-1 text-center">
+              Added this month
+            </div>
+            <div class="py-2 text-right display-2 orange--text ">
+              {{ addedThisMonth }}
+            </div>
+          </div>
+        </v-card>
+      </v-flex>
+      <v-flex xs6 sm2>
+        <v-card class="text-center pa-1 ma-3 fill-height">
+          <v-progress-circular
+            v-if="loading"
+            :size="70"
+            :width="7"
+            indeterminate
+            class="my-2"
+          ></v-progress-circular>
+          <div v-else>
+            <div class="body-1 text-center">
+              Finished {{ thisYear }}
+            </div>
+            <div class="py-2 text-right display-2 orange--text ">
+              {{ finishedThisYear }}
+            </div>
+          </div>
+        </v-card>
+      </v-flex>
+      <v-flex xs6 sm2>
+        <v-card class="text-center pa-1 ma-3 fill-height">
+          <v-progress-circular
+            v-if="loading"
+            :size="70"
+            :width="7"
+            indeterminate
+            class="my-2"
+          ></v-progress-circular>
+          <div v-else>
+            <div class="body-1 text-center">
+              Unplayed
+            </div>
+            <div class="py-2 text-right display-2 orange--text ">
+              {{ unplayed }}
+            </div>
+          </div>
+        </v-card>
+      </v-flex>
+      <v-flex xs6 sm2>
+        <v-card class="text-center pa-1 ma-3 fill-height">
+          <v-progress-circular
+            v-if="loading"
+            :size="70"
+            :width="7"
+            indeterminate
+            class="my-2"
+          ></v-progress-circular>
+          <div v-else>
+            <div class="body-1 text-center">
+              Top platforms
+            </div>
+            <div
+              v-for="(platform) in platforms"
+              :key="platform"
+              class="platformcontainer px-2 text-right orange--text">
+              <span class="platformleft">
+                <span class="hidden-lg-and-up">{{ short(platform[0]) }}</span>
+                <span class="hidden-md-and-down">{{ platform[0] }}</span>
+              </span>
+              <span class="platformright">{{ platform[1] }}</span>
+            </div>
+          </div>
         </v-card>
       </v-flex>
     </v-layout>
@@ -93,21 +182,88 @@
   </v-container>
 </template>
 <script>
-export default {
-  data() {
-    return {
+import { toDate } from '@/service/utils.js'
+import { shortPlatform } from '@/service/platforms.js'
 
-    }
-  },
+export default {
+  data: () => ({
+  }),
   computed: {
     loggedIn() { return this.$store.getters.loggedIn },
     profilePicture() { return this.$store.getters.getUser.photoURL },
     displayName() { return this.$store.getters.getUser.displayName },
     email() { return this.$store.getters.getUser.email },
     loading() { return this.$store.getters.loading },
-    collectionCount() { return this.$store.getters.getCollection.length }
+    collectionCount() { return this.$store.getters.getCollection.length },
+    thisYear() { return new Date(Date.now()).getFullYear() },
+    addedThisMonth() {
+      let collection = this.$store.getters.getCollection
+      let added = 0
+      let now = new Date(Date.now())
+      let month = now.getMonth()
+      let year = now.getFullYear()
+      collection.forEach(item => {
+        let buydate = toDate(item.buydate)
+        if (buydate.getMonth() === month && buydate.getFullYear() === year) {
+          added++
+        }
+      })
+      return added
+    },
+    addedThisYear() {
+      let collection = this.$store.getters.getCollection
+      let added = 0
+      let now = new Date(Date.now())
+      let year = now.getFullYear()
+      collection.forEach(item => {
+        let buydate = toDate(item.buydate)
+        if (buydate.getFullYear() === year) {
+          added++
+        }
+      })
+      return added
+    },
+    finishedThisYear() {
+      let collection = this.$store.getters.getCollection
+      let thisYear = new Date(Date.now()).getFullYear()
+      let finished = collection.filter(item => toDate(item.buydate).getFullYear() === thisYear && item.completed).length
+      return finished
+    },
+    unplayed() {
+      let collection = this.$store.getters.getCollection
+      return collection.filter(item => item.rating && item.rating > 0).length
+    },
+    platforms() {
+      let gamesByPlatforms = this.$store.getters.getCollection
+        .map(game => game.platform)
+        .reduce((acc, current) => {
+          // console.log(`reducing. adding ${current}`, acc)
+          if (!acc[current]) {
+            acc[current] = 0
+          }
+          acc[current] += 1
+          return {
+            ...acc
+          }
+        }, {}
+        )
+      let platforms = []
+      // transform object to array for sorting
+      for (var platform in gamesByPlatforms) {
+        if (gamesByPlatforms.hasOwnProperty(platform)) {
+          platforms.push([platform, gamesByPlatforms[platform]])
+        }
+      }
+      // sort and select top 3
+      let topPlatforms = platforms.sort((a, b) => b[1] - a[1]).slice(0, 4)
+      debugger
+      return topPlatforms
+    }
   },
   methods: {
+    short(platform) {
+      return shortPlatform(platform)
+    },
     testNotify() {
       this.$store.dispatch('notify', 'Hellooooo')
     },
@@ -120,3 +276,18 @@ export default {
   }
 }
 </script>
+<style>
+.platformcontainer {
+  width: 100%;
+}
+.platformleft {
+  width: 80%;
+  text-align: left;
+  float: left;
+}
+.platformright {
+  width: 20%;
+  text-align: right;
+  float: right;
+}
+</style>
