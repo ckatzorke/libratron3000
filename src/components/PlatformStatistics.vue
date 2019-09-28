3b391179612e34951d0a351be41973e89c37ce6d
Christian Katzorke <ckatzorke@gmail.com>
2019-09-28 20:15:56 +0200

    first chart

--- src/components/PlatformStatistics.vue
+++ src/components/PlatformStatistics.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>
+  <div id="chart">Bla
   </div>
 </template>
 <script>
+import ApexCharts from 'apexcharts'
+
+let options = {
+  chart: {
+    type: 'line'
+  },
+  series: [{
+    name: 'sales',
+    data: [30,40,35,50,49,60,70,91,125]
+  }],
+  xaxis: {
+    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+  }
+}
+
+
 export default {
   components: {
   },
@@ -39,6 +55,12 @@ export default {
       }
       return data
     }
+  },
+  mounted() {
+    var chart = new ApexCharts(document.querySelector("#chart"), options)
+
+    chart.render()
+
   }
 }
 </script>
