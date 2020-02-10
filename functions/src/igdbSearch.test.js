bffd42b7727e5417fc26a9fa190e66111d1116c0
Christian Katzorke <ckatzorke@gmail.com>
2020-02-10 13:34:54 +0100

    use env var

--- functions/src/igdbSearch.test.js
+++ functions/src/igdbSearch.test.js
@@ -1,4 +1,4 @@
 let IGDBProxy = require('./igdbSearch').IgdbProxy
 
-let client = new IGDBProxy('87f0156e4c5fb4876d7dd99ca4d01e88')
+let client = new IGDBProxy(process.env.IGDB_API_KEY)
 console.log(client.identifyGame('control'))
