bcdf429f8f804739b338b3731c8464b85d57f351
Christian Katzorke <ckatzorke@gmail.com>
2020-10-03 15:08:10 +0200

    changing to igdb api v4

--- functions/src/igdbSearch.js
+++ functions/src/igdbSearch.js
@@ -5,32 +5,34 @@ const headers = {
 }
 
 /**
-* Searches for a game on IGDB
-* @param {string} search the search string to search for (from queryStringParameters)
-* @param {number} limit the number of max results, default is 5
-* @returns {object}
-*/
+ * Searches for a game on IGDB
+ * @param {string} search the search string to search for (from queryStringParameters)
+ * @param {number} limit the number of max results, default is 5
+ * @returns {object}
+ */
 exports.handler = (event, context, callback) => {
   let search = event.queryStringParameters.search
   if (search && search.trim() !== '') {
-    const client = new IgdbProxy(process.env.IGDB_API_KEY)
-    client.identifyGame(search).then(response => {
-      callback(null, {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify(
-          { result: response }
-        )
+    const client = new IgdbProxy(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET
+    )
+    client
+      .identifyGame(search)
+      .then(response => {
+        callback(null, {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify({ result: response })
+        })
       })
-    }).catch(e => {
-      callback(null, {
-        statusCode: 503,
-        headers,
-        body: JSON.stringify(
-          { e }
-        )
+      .catch(e => {
+        callback(null, {
+          statusCode: 503,
+          headers,
+          body: JSON.stringify({ e })
+        })
       })
-    })
   } else {
     callback(null, {
       statusCode: 400,
@@ -40,33 +42,45 @@ exports.handler = (event, context, callback) => {
 }
 
 class IgdbProxy {
-  static get BASE_URL() { return 'https://api-v3.igdb.com' }
+  static get BASE_URL() {
+    return 'https://api.igdb.com/v4'
+  }
 
   /**
-   * Constructor for IgdbProxy.
-   * Previously in v2:
-   * fields: [
-    'id', 'name', 'slug', 'url', 'created_at', 'updated_at', 'summary', 'first_release_date',
-    'release_dates', 'time_to_beat', 'cover', 'screenshots', 'videos', 'websites',
-    'platforms.name', 'platforms.slug', 'platforms.url', 'platforms.logo',
-    'platforms.created_at', 'platforms.updated_at', 'genres.name', 'genres.slug', 'genres.url',
-    'genres.created_at', 'genres.updated_at'],
-    expand: ['platforms', 'genres'],
-    * @param {string} igdbApiKey the api key from idgb.com
-    */
-  constructor(igdbApiKey) {
-    this.client = axios.create({
-      queryMethod: 'url',
-      method: 'post',
-      baseURL: IgdbProxy.BASE_URL,
-      headers: {
-        Accept: 'application/json',
-        'user-key': igdbApiKey,
-        'Content-Type': 'text/plain'
-      },
-      responseType: 'json',
-      timeout: 10000
-    })
+   * Constructor for IgdbProxy. You need to call #authenticate() first before usage.
+   * @param {string} clientId the client_id from twitch developer program
+   * @param {string} clientSecret the client_secret from twitch developer program
+   */
+  constructor(clientId, clientSecret) {
+    this.clientId = clientId
+    this.clientSecret = clientSecret
+  }
+
+  async authenticate() {
+    // authenticate
+    try {
+      const response = await axios.post(
+        `https://id.twitch.tv/oauth2/token?client_id=${this.clientId}&client_secret=${this.clientSecret}&grant_type=client_credentials`
+      )
+      // create default client
+      this.accessToken = response.data.access_token
+      console.log('Authenticated, setting accessToken')
+      this.client = axios.create({
+        queryMethod: 'url',
+        method: 'post',
+        baseURL: IgdbProxy.BASE_URL,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'text/plain',
+          Authorization: `Bearer ${this.accessToken}`,
+          'Client-ID': this.clientId
+        },
+        responseType: 'json',
+        timeout: 10000
+      })
+    } catch (e) {
+      console.log('err', e)
+    }
   }
 
   async identifyGame(search) {
@@ -82,29 +96,38 @@ class IgdbProxy {
   }
 
   async getGameByName(name) {
-    const response = await this.client.post('/games', `
+    const response = await this.client.post(
+      '/games',
+      `
             fields name, url, summary, updated_at, cover.image_id, platforms.abbreviation, platforms.name, platforms.slug, genres.name, first_release_date, involved_companies.developer, involved_companies.company.name;
             limit 5;
-            where name = "${name}";`)
+            where name = "${name}";`
+    )
     return response.data
   }
 
   async getGameBySlug(slug) {
-    const response = await this.client.post('/games', `
+    const response = await this.client.post(
+      '/games',
+      `
             fields name, url, summary, updated_at, cover.image_id, platforms.abbreviation, platforms.name, platforms.slug, genres.name, first_release_date, involved_companies.developer, involved_companies.company.name;
             limit 1;
-            where slug = "${slug.toLowerCase()}";`)
+            where slug = "${slug.toLowerCase()}";`
+    )
     return response.data
   }
 
   async searchGame(searchString, limit = 10) {
-    const response = await this.client.post('/search', `
+    const response = await this.client.post(
+      '/search',
+      `
             fields game.name, game.url, game.summary, game.updated_at, game.cover.image_id, game.platforms.abbreviation, game.platforms.name, game.platforms.slug, game.genres.name, game.first_release_date, game.involved_companies.developer, game.involved_companies.company.name;
             limit ${limit};
-            search "${searchString}"; where game != null;`)
+            search "${searchString}"; where game != null;`
+    )
     const result = []
     const igdb = response.data
-    igdb.forEach((igdbEntry) => {
+    igdb.forEach(igdbEntry => {
       if (igdbEntry.game && igdbEntry.game.name) {
         result.push(igdbEntry.game)
       }
--- functions/src/igdbSearch.test.js
+++ functions/src/igdbSearch.test.js
@@ -1,4 +1,9 @@
 let IGDBProxy = require('./igdbSearch').IgdbProxy
 
-let client = new IGDBProxy(process.env.IGDB_API_KEY)
-console.log(client.identifyGame('control'))
+let client = new IGDBProxy(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
+client.authenticate().then(() => {
+  console.log('Looking for control')
+  client.searchGame('control').then(data => {
+    console.log(data)
+  })
+})
