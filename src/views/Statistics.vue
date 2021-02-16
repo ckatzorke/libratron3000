01c0a49e8ffdd068d432617894d94d567c2b48f2
Christian Katzorke <c.katzorke@web.de>
2021-02-16 12:18:05 +0100

    remove pie chart

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,17 +1,5 @@
 <template>
   <v-container grid-list-xs>
-    <v-layout row wrap px-2 justify-start>
-      <v-flex xs12>
-        <v-card  class="text-center pa-1 fill-height">
-          <v-card-title primary-title>
-            <h3 class="headline mb-0">Games by Platform</h3>
-          </v-card-title>
-          <v-card-text>
-            <lib-platformstats></lib-platformstats>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
     <v-layout row wrap justify-start>
       <v-flex xs12>
         <v-card  class="text-center fill-height">
@@ -28,11 +16,9 @@
 </template>
 
 <script>
-import PlatformStatistics from '@/components/PlatformStatistics'
 import PerYearStatistics from '@/components/PerYearStatistics'
 export default {
   components: {
-    'lib-platformstats': PlatformStatistics,
     'lib-peryear': PerYearStatistics
   },
   data() {
